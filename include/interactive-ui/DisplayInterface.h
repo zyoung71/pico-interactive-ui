d0311a240c646f042b9ce396aee2de22147037dc
zyoung71 <zyoung0071@gmail.com>
2025-12-02 04:49:52 -0500

    displayinterface update

--- include/interactive-ui/DisplayInterface.h
+++ include/interactive-ui/DisplayInterface.h
@@ -7,9 +7,10 @@ class DisplayInterface
 public:
     virtual ~DisplayInterface() = default;
 
-    virtual void DrawText(Vec2u32 pos, const char* str) = 0;
-    virtual void DrawLine(Vec2u32 pos_begin, Vec2u32 pos_end) = 0;
-    virtual void DrawPolygon(Vec2u32* points, size_t pos_count) = 0;
-    virtual void DrawSquare(Vec2u32 pos, Vec2u32 size) = 0;
-    virtual void DrawBitmap(Vec2u32 pos, void* bitmap_buff, size_t bitmap_size) = 0;
+    virtual void DrawText(Vec2u32 pos, const char* str, uint32_t color) = 0;
+    virtual void DrawPixel(Vec2u32 pos, uint32_t color) = 0;
+    virtual void DrawLine(Vec2u32 pos_begin, Vec2u32 pos_end, uint32_t color) = 0;
+    virtual void DrawPolygon(Vec2u32* points, size_t pos_count, uint32_t color) = 0;
+    virtual void DrawSquare(Vec2u32 pos, Vec2u32 size, uint32_t color, bool is_outline = true) = 0;
+    virtual void DisplayBitmap(const uint8_t* bitmap_buff, size_t bitmap_size) = 0;
 };
