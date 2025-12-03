98d9ad7c9d85bb239fbcaa7c0c5448152d252d45
zyoung71 <zyoung0071@gmail.com>
2025-12-03 01:16:29 -0500

    let user handle text alignment

--- src/interactive-ui/components/TextComponent.cpp
+++ src/interactive-ui/components/TextComponent.cpp
@@ -6,15 +6,29 @@ TextComponent::TextComponent(Screen* screen, const Vec2u32& origin, const TextPr
     : SelectableComponent(screen, origin, z_layer), text_properties(props)
 {
     size_t msg_len = strlen(props.text);
-    message_pixel_length = (msg_len * props.width + (msg_len - 1) * props.spacing) / props.lines;
-    draw_dimensions = {message_pixel_length / props.lines, props.height * props.lines};
+    text_properties.lines = 1;
+    for (size_t c = 0; c < msg_len; c++) //  <---- DID YOU SEE THAT???
+    {
+        if (props.text[c] == '\n')
+            text_properties.lines++;
+    }
+
+    size_t message_pixel_length = (msg_len * props.width + (msg_len - 1) * props.spacing) / props.lines;
+    draw_dimensions = {message_pixel_length, props.height * props.lines + props.spacing * (props.lines - 1)};
 }
 TextComponent::TextComponent(Screen* screen, float x_percentage, float y_percentage, const TextProperties& props, int32_t z_layer)
     : SelectableComponent(screen, x_percentage, y_percentage, z_layer), text_properties(props)
 {
     size_t msg_len = strlen(props.text);
-    message_pixel_length = (msg_len * props.width + (msg_len - 1) * props.spacing) / props.lines;
-    draw_dimensions = {message_pixel_length / props.lines, props.height * props.lines + props.spacing * (props.lines - 1)};
+    text_properties.lines = 1;
+    for (size_t c = 0; c < msg_len; c++)
+    {
+        if (props.text[c] == '\n')
+            text_properties.lines++;
+    }
+    
+    size_t message_pixel_length = (msg_len * props.width + (msg_len - 1) * props.spacing) / props.lines;
+    draw_dimensions = {message_pixel_length, props.height * props.lines + props.spacing * (props.lines - 1)};
 }
 
 void TextComponent::Draw()
@@ -32,65 +46,21 @@ TextBoxComponent::TextBoxComponent(Screen* screen, const Vec2u32& origin, const 
     : TextComponent(screen, origin, props, z_layer), padding(padding)
 {
     draw_dimensions = dimensions;
-    Vec2u32 range_local = draw_dimensions - padding * 2; // maximum pixel length/height for text
-    text_properties.lines = message_pixel_length / range_local.x;
-
-    if (message_pixel_length % range_local.x > 0) // If doesn't fit perfectly
-        text_properties.lines++; // add a line
-
-    size_t height_per_line = text_properties.height + text_properties.spacing;
-    if (height_per_line > range_local.y / text_properties.lines) // clamp if height conflicts
-    {
-        text_properties.lines = range_local.y / height_per_line;
-    }
 }
 TextBoxComponent::TextBoxComponent(Screen* screen, float x_percentage, float y_percentage, const Vec2u32& dimensions, const Vec2u32& padding, const TextProperties& props, int32_t z_layer)
     : TextComponent(screen, x_percentage, y_percentage, props, z_layer), padding(padding)
 {
     draw_dimensions = dimensions;
-    Vec2u32 range_local = draw_dimensions - padding * 2; // maximum pixel length/height for text
-    text_properties.lines = message_pixel_length / range_local.x;
-
-    if (message_pixel_length % range_local.x > 0) // If doesn't fit perfectly
-        text_properties.lines++; // add a line
-
-    size_t height_per_line = text_properties.height + text_properties.spacing;
-    if (height_per_line > range_local.y / text_properties.lines) // clamp if height conflicts
-    {
-        text_properties.lines = range_local.y / height_per_line;
-    }
 }
 TextBoxComponent::TextBoxComponent(Screen* screen, const Vec2u32& origin, const Vec2u32& dimensions, float x_pad_percentage, float y_pad_percentage, const TextProperties& props, int32_t z_layer)
     : TextComponent(screen, origin, props, z_layer), padding({static_cast<uint32_t>(dimensions.x * x_pad_percentage), static_cast<uint32_t>(dimensions.y * y_pad_percentage)})
 {
     draw_dimensions = dimensions;
-    Vec2u32 range_local = draw_dimensions - padding * 2; // maximum pixel length/height for text
-    text_properties.lines = message_pixel_length / range_local.x;
-
-    if (message_pixel_length % range_local.x > 0) // If doesn't fit perfectly
-        text_properties.lines++; // add a line
-
-    size_t height_per_line = text_properties.height + text_properties.spacing;
-    if (height_per_line > range_local.y / text_properties.lines) // clamp if height conflicts
-    {
-        text_properties.lines = range_local.y / height_per_line;
-    }
 }
 TextBoxComponent::TextBoxComponent(Screen* screen, float x_percentage, float y_percentage, const Vec2u32& dimensions, float x_pad_percentage, float y_pad_percentage, const TextProperties& props, int32_t z_layer)
     : TextComponent(screen, x_percentage, y_percentage, props, z_layer), padding({static_cast<uint32_t>(dimensions.x * x_pad_percentage), static_cast<uint32_t>(dimensions.y * y_pad_percentage)})
 {
     draw_dimensions = dimensions;
-    Vec2u32 range_local = draw_dimensions - padding * 2; // maximum pixel length/height for text
-    text_properties.lines = message_pixel_length / range_local.x;
-
-    if (message_pixel_length % range_local.x > 0) // If doesn't fit perfectly
-        text_properties.lines++; // add a line
-
-    size_t height_per_line = text_properties.height + text_properties.spacing;
-    if (height_per_line > range_local.y / text_properties.lines) // clamp if height conflicts
-    {
-        text_properties.lines = range_local.y / height_per_line;
-    }
 }
 
 void TextBoxComponent::Draw()
