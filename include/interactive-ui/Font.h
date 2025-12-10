216443d533d33aa59d1876b88ef0c1695bf56dab
zyoung71 <zyoung0071@gmail.com>
2025-12-10 02:49:30 -0500

    font ascii range

--- include/interactive-ui/Font.h
+++ include/interactive-ui/Font.h
@@ -12,8 +12,11 @@ struct Font
     const uint8_t char_spacing;
     const ArrayView<uint8_t> char_bitmap;
 
-    consteval Font(uint8_t char_height, uint8_t char_width, uint8_t char_spacing, const uint8_t* char_bmp_data, size_t bmp_data_size)
-        : char_height(char_height), char_width(char_width), char_spacing(char_spacing), char_bitmap(make_array_view(char_bmp_data, bmp_data_size))
+    const uint8_t ascii_begin;
+    const uint8_t ascii_end;
+
+    consteval Font(uint8_t char_height, uint8_t char_width, uint8_t char_spacing, uint8_t ascii_begin, uint8_t ascii_end, const uint8_t* char_bmp_data, size_t bmp_data_size)
+        : char_height(char_height), char_width(char_width), char_spacing(char_spacing), ascii_begin(ascii_begin), ascii_end(ascii_end), char_bitmap(make_array_view(char_bmp_data, bmp_data_size))
     {
     }
 
@@ -22,12 +25,12 @@ struct Font
     template<uint8_t char_height, uint8_t char_width, uint8_t char_spacing, uint8_t ascii_begin = 32, uint8_t ascii_end = 126>
     static consteval Font BuildFont(const uint8_t data[char_width * (ascii_end - ascii_begin)])
     {
-        return Font(char_height, char_width, char_spacing, data, char_width * (ascii_end - ascii_begin));
+        return Font(char_height, char_width, char_spacing, ascii_begin, ascii_end, data, char_width * (ascii_end - ascii_begin));
     }
 
     template<uint8_t char_height, uint8_t char_width, uint8_t char_spacing, uint8_t ascii_begin = 32, uint8_t ascii_end = 126>
     static consteval Font BuildFont(const uint8_t data[ascii_end - ascii_begin][char_width])
     {
-        return Font(char_height, char_width, char_spacing, data, char_width * (ascii_end - ascii_begin));
+        return Font(char_height, char_width, char_spacing, ascii_begin, ascii_end, data, char_width * (ascii_end - ascii_begin));
     }
 };
